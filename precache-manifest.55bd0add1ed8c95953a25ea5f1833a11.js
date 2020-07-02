826ce79ca8fa7088af87014ad469f5b1199ea1f6
Gabriel Seals <sealsgj@gmail.com>
2020-07-02 14:17:21 -0500

    Updates

---
+++ precache-manifest.55bd0add1ed8c95953a25ea5f1833a11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a365bbd82079079d555c1327738860",
+    "url": "/React-Movie-Database/index.html"
+  },
+  {
+    "revision": "a748071780ffec442bb5",
+    "url": "/React-Movie-Database/static/css/main.a55a38e8.chunk.css"
+  },
+  {
+    "revision": "0e12b14f7b92e5e94f87",
+    "url": "/React-Movie-Database/static/js/2.1a7f6c37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Movie-Database/static/js/2.1a7f6c37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a748071780ffec442bb5",
+    "url": "/React-Movie-Database/static/js/main.9210cf56.chunk.js"
+  },
+  {
+    "revision": "c616564315c7822a9edb",
+    "url": "/React-Movie-Database/static/js/runtime-main.f7436f2b.js"
+  },
+  {
+    "revision": "ddcc435b86696553169763cca46f23e1",
+    "url": "/React-Movie-Database/static/media/film3.ddcc435b.jpg"
+  }
+]);
